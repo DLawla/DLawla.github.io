1f9576bc8ffd4c9c228ebcd13a3f70e265701981
Dan Lawler <dan.r.lawler@gmail.com>
2025-02-26 20:17:04 +1300

    updating v2.0 2025

--- index.jsx
+++ index.jsx
@@ -1,21 +1,22 @@
 const stateNames =
   [
     "splash",
-    "question1", // parent names
-    "question2", // date of first date
-    "question3", // date of conception
-    "question4", // vehicle rego
-    "question5", // favorite child names
-    "question6", // favorite music
-    "question7", // parenting priorities
-    "question8", // sex preference
-    "question9", // moon question
+    "question200", // parent names
+    "question300", // parent names
+    "question400", // date of first date
+    "question500", // date of conception
+    "question550", // vehicle rego
+    "question600", // favorite child names
+    "question700", // favorite music
+    "question800", // parenting priorities
+    "question900", // sex preference
+    "question100", // moon question
     "calculationsPrep", // click to advance
-    "calculations", // click to advance
+    "calculations", // cals
     "reveal"
   ]
 
-const gender = 1
+const gender = 0
 
 const inputStyle = "appearance-none w-full my-8 bg-gray-50 border border-green-300 text-green-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
 
@@ -66,13 +67,15 @@ const App = (props) => {
 
         <p className={"pb-8"}>Within thousands of milliseconds, you can know your fresh human gender!</p>
 
-        <div>
+        <div className={"pb-16"}>
           <button onClick={onAdvance}>Get started</button>
         </div>
+
+        <p className={"text-xs text-gray-500"}>V2.04, powered by the latest human mass surveillance efforts and Reddit posts.</p>
       </div>
     );
   }
-  else if (stateNames[appState] === "question1") {
+  else if (stateNames[appState] === "question200") {
     return (
       <div className="text-center text-green-500">
         <h1 className={"text-xl pb-8"}>Question 1</h1>
@@ -84,12 +87,11 @@ const App = (props) => {
       </div>
     );
   }
-  else if (stateNames[appState] === "question2") {
+  else if (stateNames[appState] === "question300") {
     return (
       <div className="text-center text-green-500">
         <h1 className={"text-xl pb-8"}>Question 10</h1>
         <p className={"mb-4"}>What is the human male parent name?</p>
-        {/*// Wrapped div to clear input*/}
         <div>
           <input for={stateNames[appState]} id={stateNames[appState]} className={inputStyle}></input>
         </div>
@@ -99,7 +101,7 @@ const App = (props) => {
       </div>
     );
   }
-  else if (stateNames[appState] === "question3") {
+  else if (stateNames[appState] === "question400") {
     return (
       <div className="text-center text-green-500">
         <h1 className={"text-xl pb-8"}>Question 11</h1>
@@ -111,7 +113,7 @@ const App = (props) => {
       </div>
     );
   }
-  else if (stateNames[appState] === "question4") {
+  else if (stateNames[appState] === "question500") {
     return (
       <div className="text-center text-green-500">
         <h1 className={"text-xl pb-8"}>Question 100</h1>
@@ -125,12 +127,24 @@ const App = (props) => {
       </div>
     );
   }
-  else if (stateNames[appState] === "question5") {
-    const names = ["Doug", "Sharon", "Phil", "Karen"]
-
+  else if (stateNames[appState] === "question550") {
     return (
       <div className="text-center text-green-500">
         <h1 className={"text-xl pb-8"}>Question 101</h1>
+        <p className={"mb-4"}>What is the approximate time of day of conception of non-gendered fresh human?</p>
+        <div className={"pb-8"}>[Tolerance +/- 0.5 minutes]</div>
+        <input type="time" defaultValue="12:00" className={inputStyle}/>
+        <div>
+          <button onClick={onAdvance}>Next</button>
+        </div>
+      </div>
+    );
+  } else if (stateNames[appState] === "question600") {
+    const names = ["Sebastian", "Carol", "Chad"]
+
+    return (
+      <div className="text-center text-green-500">
+        <h1 className={"text-xl pb-8"}>Question 111</h1>
         <p className={"mb-4"}>What are your favorite small human names?</p>
         <div>
           {
@@ -151,12 +165,12 @@ const App = (props) => {
       </div>
     );
   }
-  else if (stateNames[appState] === "question6") {
-    const names = ["Fiddy cent", "Ukrainian rave", "Talkback radio", "Angsty rock", "Nickelback"]
+  else if (stateNames[appState] === "question700") {
+    const names = ["8-bit", "Mariachi", "Ukrainian rave", "Talkback radio", "Elevator", "Birdsong", "Shaggy - 'It was me'", "Nickelback"]
 
     return (
       <div className="text-center text-green-500">
-        <h1 className={"text-xl pb-8"}>Question 110</h1>
+        <h1 className={"text-xl pb-8"}>Question 1000</h1>
         <p className={"mb-4"}>What are your favorite types of human music?</p>
 
         <div>
@@ -182,13 +196,13 @@ const App = (props) => {
       </div>
     );
   }
-  else if (stateNames[appState] === "question7") {
-    const names = ["Bathing your child", "First table reservations"]
+  else if (stateNames[appState] === "question800") {
+    const names = ["CAD skills", "ROI", "New tax dependent", "Excuse to leave social engagement early"]
 
     return (
       <div className="text-center text-green-500">
-        <h1 className={"text-xl pb-8"}>Question 111</h1>
-        <p className={"mb-4"}>As future successful breeders, what will be your highest priority as a parent?</p>
+        <h1 className={"text-xl pb-8"}>Question 1001</h1>
+        <p className={"mb-4"}>As successful breeders, what do you value most in a baby human?</p>
 
         <div>
           {
@@ -209,12 +223,12 @@ const App = (props) => {
       </div>
     );
   }
-  else if (stateNames[appState] === "question8") {
-    const names = ["Male", "Female", "YOLO", "Who cares?", "Hilux"]
+  else if (stateNames[appState] === "question900") {
+    const names = ["Male", "Female", "YOLO", "Who cares?", "Tesla", "Hilux"]
 
     return (
       <div className="text-center text-green-500">
-        <h1 className={"text-xl pb-8"}>Question 1000</h1>
+        <h1 className={"text-xl pb-8"}>Question 1010</h1>
         <p className={"mb-4"}>What is your sex preference for your future baby?</p>
 
         <div>
@@ -236,7 +250,7 @@ const App = (props) => {
       </div>
     );
   }
-  else if (stateNames[appState] === "question9") {
+  else if (stateNames[appState] === "question100") {
     const names = ["384000km", "1 AU", "239000mi", "I don’t care"]
 
     return (
@@ -315,23 +329,25 @@ const GenderCalculation = ({onAdvance, showMatrix, hideMatrix}) => {
   }
 
   let startingData = [
-    { bgcolor: "green", title: "Penis size", progress: 80, units: "cm" },
-    { bgcolor: randomColor(), title: "VO2 max", progress: 53 },
+    { bgcolor: "green", title: "Penis size", progress: 22, units: "cm" },
+    { bgcolor: randomColor(), title: "Cuteness rating", progress: 92 },
+    { bgcolor: randomColor(), title: "Femur sheer strength", progress: 30, units: "MPa" },
+    { bgcolor: randomColor(), title: "Git commit frequency", progress: 53, units: "/day" },
+    { bgcolor: randomColor(), title: "Child actor probability", progress: 2 },
+    { bgcolor: randomColor(), title: "Drool conductivity", progress: 78, units: "S/m" },
+    { bgcolor: randomColor(), title: "Sleep per night", progress: 53, units: "minutes" },
     { bgcolor: randomColor(), title: "Volume of crying", progress: 30, units: "dB" },
-    { bgcolor: randomColor(), title: "Sleep per night", progress: 53, units: "minutes"  },
-    { bgcolor: randomColor(), title: "Love of maple syrup", progress: 53 },
-    { bgcolor: randomColor(), title: "Understanding of thermodynamics", progress: 53, units: "Q" },
-    { bgcolor: randomColor(), title: "Number of green fingers", progress: 53, units: "digits" },
-    { bgcolor: randomColor(), title: "Patience", progress: 53 },
-    { bgcolor: randomColor(), title: "Teenage fringe length", progress: 53, units: "mm" },
-    { bgcolor: randomColor(), title: "Age of first IF/ELSE statement", progress: 53, units: "months" },
-    { bgcolor: randomColor(), title: "Foot ugliness", progress: 53 },
     { bgcolor: randomColor(), title: "Mouth breather rating", progress: 53 },
+    { bgcolor: randomColor(), title: "Typing speed", progress: 70, units: "WPM" },
+    { bgcolor: randomColor(), title: "Chest hair", progress: 2 },
+    { bgcolor: randomColor(), title: "VO2 max", progress: 82 },
+    { bgcolor: randomColor(), title: "Probability of saying 'dada' first", progress: 59, units: "%" }
   ];
 
   const [overallProgress, setOverallProgress] = React.useState(0)
   const [data, setData] = React.useState(startingData);
   const [complete, setComplete] = React.useState(false);
+  const [finalAnimation, setFinalAnimation] = React.useState(false);
 
   useInterval(() => {
     const newData = []
@@ -378,27 +394,41 @@ const GenderCalculation = ({onAdvance, showMatrix, hideMatrix}) => {
       let clonedData = data.map(a => {return {...a}})
 
       if (gender == 0) {
-        clonedData[0].progress = 0.0
-        setData(clonedData)
+        clonedData[0].progress = 25.0
       } else if (gender == 1) {
         clonedData[0].progress = 100.0
-        setData(clonedData)
       }
+      setData(clonedData)
 
       clonedData = data.map(a => {return {...a}})
       setTimeout(() => {
         if (gender == 0) {
-          clonedData[0].progress = 0.0
+          clonedData[0].progress = 95.0
         } else if (gender == 1) {
           clonedData[0].progress = 100.0
         }
-        clonedData[0].extraClass = "animate-ping"
+        clonedData[0].extraClass = "animate-bounce"
         setData(clonedData)
       }, 2000)
 
-      setTimeout(onAdvance, 5000)
+      setTimeout(() => setFinalAnimation(true), 5000)
     }
   }, [complete])
+
+  React.useEffect(() => {
+    if (finalAnimation) {
+      let clonedData = data.map(a => {return {...a}})
+      if (gender == 0) {
+        clonedData[0].progress = 0.0
+      } else if (gender == 1) {
+        clonedData[0].progress = 100.0
+      }
+      clonedData[0].extraClass = "animate-bounce"
+      setData(clonedData)
+
+      setTimeout(onAdvance, 3000)
+    }
+  }, [finalAnimation])
 
   return (
     <div className="text-center text-green-500">
@@ -418,11 +448,13 @@ const Reveal = () => {
   const [reveal1, setReveal1] = React.useState(false)
   const [reveal2, setReveal2] = React.useState(false)
   const [reveal3, setReveal3] = React.useState(false)
+  const [reveal4, setReveal4] = React.useState(false)
 
   React.useEffect(() => {
     setTimeout(() => {setReveal1(true)}, 2000)
     setTimeout(() => {setReveal2(true)}, 4000)
     setTimeout(() => {setReveal3(true)}, 6000)
+    setTimeout(() => {setReveal4(true)}, 8000)
   }, [])
 
   return (
@@ -438,15 +470,23 @@ const Reveal = () => {
       }
 
       {reveal2 &&
-        <h1 className={"text-3xl pb-8"}>
+        <h1 className={"text-3xl pb-20"}>
           ({gender == 0 ? "GIRL" : "BOY"})
         </h1>
       }
 
       {reveal3 &&
-        <h1 className={"text-3xl pb-8 animate-ping"}>
-          GONGRATS!
-        </h1>
+        <div className={"pb-32"}>
+          <h1 className={`text-3xl ${reveal4 ? "animate-spin" : "animate-bounce"}`}>
+            CONGRATS!
+          </h1>
+        </div>
+      }
+
+      {reveal4 &&
+        <div className={"text-sm text-gray-800"}>
+          Don't like your results? Refresh the page or try again next year.
+        </div>
       }
     </div>
   );
